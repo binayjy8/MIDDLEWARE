c172e85f849f607c33d844596202b24afedd15d4
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-06-24 20:01:20 +0530

    backend

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const app = express();
 
-app.use( (req, res, next) => {
-    console.log("i'm the 1st middleware");
-    next();
-});
+// app.use( (req, res, next) => {
+//     console.log("i'm the 1st middleware");
+//     next();
+// });
 
-app.use( (req, res, next) => {
-    console.log("i'm the 2nd middleware");
-    next();
-});
+// app.use( (req, res, next) => {
+//     console.log("i'm the 2nd middleware");
+//     next();
+// });
+
+
 
 app.get("/", (req, res) => {
     res.send("hi, i'm the root");
@@ -17,6 +19,16 @@ app.get("/", (req, res) => {
 
 app.get("/random", (req, res) => {
     res.send("this is a random page");
+});
+
+app.use( (req, res, next) => {
+    req.time = Date.now();
+    console.log(req.method, req.hostname, req.path, req.time);
+    next();
+});
+
+app.use((req, res) => {
+    res.send("page not found");
 });
 
 app.listen(8080, (req, res) => {
