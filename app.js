8cbd891cb75192083070b35a7a73f8c816c72d04
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-06-24 15:56:13 +0530

    backend

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+app.get("/", (req, res) => {
+    res.send();
+});
+
 app.listen(8080, (req, res) => {
     console.log("server  listening to the port 8080");
 });
