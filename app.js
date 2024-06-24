7f93d6ba00f04ae3e674621dff5add46630cf388
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-06-24 16:01:24 +0530

    backend

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const app = express();
 
+app.use( () => {
+    console.log("i'm the middleware");
+});
+
 app.get("/", (req, res) => {
-    res.send();
+    res.send("hi, i'm the root");
+});
+
+app.get("/random", (req, res) => {
+    res.send("this is a random page");
 });
 
 app.listen(8080, (req, res) => {
