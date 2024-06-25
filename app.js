1239c958ae815e788c925294ecef25810492f012
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-06-25 07:39:12 +0530

    backend

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
 const express = require("express");
 const app = express();
 
-// app.use( (req, res, next) => {
-//     console.log("i'm the 1st middleware");
-//     next();
-// });
-
-// app.use( (req, res, next) => {
-//     console.log("i'm the 2nd middleware");
-//     next();
-// });
-
-
+app.get("/api", (req, res) => {
+    res.send("data");
+});
 
 app.get("/", (req, res) => {
     res.send("hi, i'm the root");
@@ -21,11 +13,11 @@ app.get("/random", (req, res) => {
     res.send("this is a random page");
 });
 
-app.use( (req, res, next) => {
-    req.time = Date.now();
-    console.log(req.method, req.hostname, req.path, req.time);
-    next();
-});
+// app.use( (req, res, next) => {
+//     req.time = Date.now();
+//     console.log(req.method, req.hostname, req.path, req.time);
+//     next();
+// });
 
 app.use((req, res) => {
     res.send("page not found");
